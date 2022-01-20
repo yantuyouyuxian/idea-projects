34665d1cd228cf6dae0046873dd0d810c9d31962
zhangHan <2366120116@qq.com>
2022-01-20 10:52:06 +0800

    cc

---
+++ my-projects/leetcode/src/main/java/leetcode/LeetCode219.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/1/19 21:03
+ * @ Description
+ */
+public class LeetCode219 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        LeetCode219 lc = new LeetCode219();
+        System.out.println(lc.containsNearbyDuplicate(nums, k));
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate_2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ my-projects/leetcode/src/main/java/leetcode/LeetCode7.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/1/19 21:46
+ * @ Description
+ */
+public class LeetCode7 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int reverse(int x) {
+        return 0;
+    }
+
+}
