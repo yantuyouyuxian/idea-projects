f0057733c7d25db04d20df8677ad1febca77185b
zhangHan <2366120116@qq.com>
2022-01-29 22:52:38 +0800

    common

---
+++ my-projects/jvm/src/main/java/StringTableTest.java
@@ -0,0 +1,14 @@
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/1/27 21:53
+ * @ Description
+ */
+public class StringTableTest {
+    public static void main(String[] args) {
+        int a =1;
+        String s1 = new String("a");
+        String s4 = new String("b");
+        String s2 = new String("ab");
+        String s3 = "ab";
+    }
+}
---
+++ my-projects/leetcode/src/main/java/leetcode/LeetCode1668.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import org.junit.Test;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/1/25 15:30
+ * @ Description
+ */
+public class LeetCode1668 {
+    public static void main(String[] args) {
+        LeetCode1668 lc = new LeetCode1668();
+        System.out.println(lc.numberOfMatches(14));
+        System.out.println(lc.numberOfMatches(7));
+        System.out.println(lc.numberOfMatches(1));
+        System.out.println(lc.numberOfMatches(2));
+        System.out.println(lc.numberOfMatches(3));
+        System.out.println(lc.numberOfMatches(4));
+    }
+
+    public int numberOfMatches(int n) {
+        int res = 0;
+        while (n != 1) {
+            res += n / 2;
+            n = (n & 1) + (n >> 1);
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        int a = 13;
+        System.out.println(a & 1);
+    }
+}
