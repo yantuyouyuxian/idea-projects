a2139324def8a554446ac378f95df2907805fc1d
zhangHan <2366120116@qq.com>
2022-02-10 22:17:54 +0800

    cc

---
+++ my-projects/spring-boot-test/src/main/java/com/zh/WebApplication.java
@@ -0,0 +1,16 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/2/10 22:13
+ * @ Description
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
