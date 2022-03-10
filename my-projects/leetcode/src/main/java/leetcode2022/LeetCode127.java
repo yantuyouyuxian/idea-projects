d102bd29c65c782714e7e35bdf6b1945f9b749e6
zhangHan <2366120116@qq.com>
2022-03-10 09:09:38 +0800

    common

---
+++ my-projects/leetcode/src/main/java/leetcode2022/LeetCode127.java
@@ -0,0 +1,62 @@
+package leetcode2022;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/3/9 23:29
+ * @ Description
+ */
+public class LeetCode127 {
+    public static void main(String[] args) {
+        LeetCode127 lc = new LeetCode127();
+        List<String> words = Arrays.asList("hot", "dot", "dog", "lot", "log");
+        System.out.println(lc.ladderLength("hit", "cog", words));
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> curStrList = new HashSet<>();
+        curStrList.add(beginWord);
+        return findStr(curStrList, wordList, 1, endWord);
+    }
+
+    public int findStr(Set<String> curStrList, List<String> others, int count, String endWord) {
+        if (curStrList.contains(endWord)) {
+            return count;
+        }
+        boolean goNext = false;
+        HashSet<String> nextBeginList = new HashSet<>();
+        for (String cur : curStrList) {
+            for (String str : others) {
+                if (distinctOnlyOne(cur, str)) {
+                    nextBeginList.add(str);
+                    goNext = true;
+                }
+            }
+        }
+        List<String> nextOthers = others.stream().filter(e -> !nextBeginList.contains(e)).collect(Collectors.toList());
+        if (goNext) {
+            return findStr(nextBeginList, nextOthers, count + 1, endWord);
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean distinctOnlyOne(String a, String b) {
+        int count = 0;
+        if (a.equals(b)) {
+            return false;
+        } else {
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i) != b.charAt(i)) {
+                    count++;
+                }
+            }
+        }
+        return count == 1;
+    }
+}
---
+++ my-projects/leetcode/src/main/java/leetcode2022/LeetCode151.java
@@ -0,0 +1,27 @@
+package leetcode2022;
+
+/**
+ * @ Author         zhangHan
+ * @ Date           2022/3/9 23:00
+ * @ Description
+ */
+public class LeetCode151 {
+    public static void main(String[] args) {
+        LeetCode151 lc = new LeetCode151();
+        System.out.println(lc.reverseWords("  Bob    Loves  Alice   "));
+        System.out.println(lc.reverseWords("Alice does not even like bo"));
+    }
+
+    public String reverseWords(String s) {
+        String[] strList = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strList.length - 1; i >= 0; i--) {
+            if (strList[i].equals("")) {
+                continue;
+            }
+            sb.append(strList[i]);
+            sb.append(" ");
+        }
+        return sb.substring(0, sb.length() - 1);
+    }
+}
